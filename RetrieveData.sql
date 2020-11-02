9c083664840afabea44a028d6b13c394c4bfc2b3
Priya Biriya <priyabiriya1707@gmail.com>
2020-11-03 02:13:06 +0530

    [Priya] add. retrieve data

---
+++ RetrieveData.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE payroll_service
+USE payroll_service;
+SELECT DB_NAME()
+
+--UC2
+create table employee_payroll
+(
+ID int identity(1,1),
+Fname varchar(25) not null,
+Salary money not null,
+startd date not null
+);
+
+select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'employee_payroll';
+insert into employee_payroll values
+('Bill',100000.00,'2018-01-03'),
+('Terissa',200000.00,'2019-11-13'),
+('Charlie',300000.00,'2020-05-21');
+select * from employee_payroll;
