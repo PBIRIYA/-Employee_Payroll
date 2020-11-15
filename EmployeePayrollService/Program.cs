312dfce4256ad7dae61b293ab31b70e9eeef2af4
Priya Biriya <priyabiriya1707@gmail.com>
2020-11-15 15:31:06 +0530

    [Priya] add files

--- EmployeePayrollService/Program.cs
+++ EmployeePayrollService/Program.cs
@@ -91,6 +91,13 @@ namespace EmployeePayrollService
                         repo.CountOfEmployeesGenderWise();
                         break;
                     case 11:
+                        Console.WriteLine("Enter employee Id of employee to be removed");
+                        int empId = Convert.ToInt32(Console.ReadLine());
+                        bool employeeRemoved = repo.RemoveEmployee(empId);
+                        if (employeeRemoved)
+                            Console.WriteLine("Employee Removed successfully");
+                        break;
+                    case 12:
                         loop = 0;
                         break;
                 }
